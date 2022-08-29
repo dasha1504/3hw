76656bb6e0cb326597ab454fdc54c6f6331e1426
dasha <dasha15042009@yandex.ru>
2022-08-29 21:17:56 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine());
+int a1 = n/10000;
+int a2 = n%10;
+int n1 = (n/10)%1000;
+int b1 = n1/100;
+int b2 = n1%10;
+if(a1 == a2 && b1 == b2)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("NO");
+}
+
